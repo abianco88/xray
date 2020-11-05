8a50b48f58cb4509772688f17d0622af2bdbded6
abianco88 <aniello.bianco@gmail.com>
2020-11-05 15:19:38 -0600

    Addition of Unify reporting to DataPrep

--- src/CDMADataPrep._v0.jl
+++ src/CDMADataPrep._v0.jl
@@ -6,7 +6,7 @@
 # Julia v0.5.2
 module CDMADataPrep
 
-using Base.Dates, DataFrames, DataStructures, CLib, IRIfunc, XLib   # CLib needed for `rf`; IRIfunc needed for `CFGwf`; XLib needed for `strdf`
+using Base.Dates, DataFrames, DataStructures, CLib, IRIfunc, XLib, CDMAReporting   # CLib needed for `rf`; IRIfunc needed for `CFGwf`; XLib needed for `strdf`; CDMAReporting is needed for `format_unify_reports_prep`
 
 function pre_data_check(pre_qc_data::Array{String,1}, cfg::OrderedDict)
     res = true
@@ -130,7 +130,7 @@ function base_dataprep(isenc::Bool, flag::Int64=1)
 
     dfmx, dfd, brand_data, upc_data, hhcounts_date, buyer_week_data, imp_week = cdma_dataprep(cfg, brand_data, upc_data, hhcounts_date, buyer_week_data, imp_week, descDump, src, flag);
 
-    return dfd, brand_data, upc_data, hhcounts_date, buyer_week_data, imp_week
+    return dfd, brand_data, upc_data, hhcounts_date, buyer_week_data, imp_week, cfg
 end
 
 function freq_dataprep(hhcounts_date::DataFrame, buyer_week_data::DataFrame) # Need to be reviewed and possibly changed
@@ -215,6 +215,122 @@ function freq_dataprep(hhcounts_date::DataFrame, buyer_week_data::DataFrame) # N
     names!(freq_index, [:Exposures, :frq_index]);
 
     return pur_data1, exp_data1, exp_data_n1, combined, freq_index, expocnt # Only `combined`, `freq_index`, `expocnt` needed in other functions
+end
+
+function format_unify_reports_prep(flag::Int64=1)
+    dfd, brand_data, upc_data, hhcounts_date, buyer_week_data, imp_week, cfg = base_dataprep(false, flag);
+    pur_data1, exp_data1, exp_data_n1, combined, freq_index, expocnt = freq_dataprep(hhcounts_date, buyer_week_data);
+
+    # Read the 2 needed values from `scored.csv`
+    scored = readtable(cfg[:files][:scored]);
+    udj_avg_expsd_pst = scored[(scored[:MODEL_DESC] .== "Total Campaign") & (scored[:dependent_variable] .== "pen"), :UDJ_AVG_EXPSD_HH_PST][1];
+    udj_avg_cntrl_pst = scored[(scored[:MODEL_DESC] .== "Total Campaign") & (scored[:dependent_variable] .== "pen"), :UDJ_AVG_CNTRL_HH_PST][1];
+
+    dfo_Exp, dfo_UnExp = CDMAReporting.genbuyerclass(dfd, udj_avg_expsd_pst, udj_avg_cntrl_pst);
+    dfd_rpt_trl = CDMAReporting.gentrialrepeat(dfd, udj_avg_expsd_pst, udj_avg_cntrl_pst);
+    dfd_fr_shr_ndx = CDMAReporting.fairshare(dfd, brand_data);
+    shr_rqrmnt = CDMAReporting.targetshare_of_category(dfd);
+    dfd_upc_grwth_cnt = CDMAReporting.upc_growth(dfd, upc_data);
+    Frst_Buy_Frq_Dgtl_std, Frst_Buy_Frq_Dgtl_Dyn = CDMAReporting.freq_HH_Cum1stpur(combined, freq_index);
+    Tm_1st_by_lst_xpsur_Dgtl = CDMAReporting.first_buy_last_exp(combined);
+    exposed_buyer_by_week, cumulative_by_week = CDMAReporting.Buyer_Frequency_Characteristics(hhcounts_date, dfd);
+
+    return Tm_1st_by_lst_xpsur_Dgtl, dfo_Exp, dfo_UnExp, dfd_rpt_trl, dfd_fr_shr_ndx, dfd_upc_grwth_cnt, imp_week, exposed_buyer_by_week, shr_rqrmnt, Frst_Buy_Frq_Dgtl_std, cfg
+end
+
+function format_unify_reports(Tm_1st_by_lst_xpsur_Dgtl::DataFrame, dfo_Exp::DataFrame, dfo_UnExp::DataFrame, dfd_rpt_trl::DataFrame,  dfd_fr_shr_ndx::DataFrame, dfd_upc_grwth_cnt::DataFrame, imp_week::DataFrame, exposed_buyer_by_week::DataFrame, shr_rqrmnt::DataFrame, Frst_Buy_Frq_Dgtl_std::DataFrame, cfg, ChannelCode::Int64, flag::Int64=1)  # Need to be reviewed and possibly changed
+    # flag = 1 -> Execute a block of code
+    Lift_Buyer_char_template = DataFrame(model_desc = String[], model = String[], time_agg_period = Int64[], start_week = Int64[], end_week = Int64[], characteristics_indicator_flag = String[], cum_num_cat_buyer = Float64[], cum_num_brd_shift_buyer = Float64[], cum_num_brd_buyer = Float64[], cum_num_lapsed_buyer = Float64[], cum_total_buyer = Float64[], pct_tot_hh = Float64[], pct_buy_hh = Float64[], cum_num_new_buyer = Int64[], cum_non_brd = Int64[], cum_num_repeat = Int64[], cum_num_cat_shift_buyer = Int64[], cum_pct_repeat_expsd = Float64[], cum_pct_trail_expsd = Float64[], impression_count = Int64[], cumulatve_hh_count = Int64[], channel_code = Int64[]);
+    start_week = parse(Int64, cfg[:start_week]);
+    end_week = parse(Int64, cfg[:end_week]);
+    Time_1st_buy = deepcopy(Tm_1st_by_lst_xpsur_Dgtl);
+    FREQ = "FRQ";
+   
+    if cfg[:campaign_type] == :lift || cfg[:campaign_type] == :SamsClub  || cfg[:campaign_type] == :digitallift || cfg[:campaign_type] == :digitalliftuat || cfg[:campaign_type] == :tvlift || cfg[:campaign_type] == :tvliftuat
+        for i in 1:(nrow(Time_1st_buy)-1)
+            if i == 10
+                push!(Lift_Buyer_char_template, [string("10+"),"FRQ1388",end_week-start_week+1,start_week,start_week+i-1,"BUYER_CHAR_WEEK_FREQUENCY",0,0,0,0,0,Frst_Buy_Frq_Dgtl_std[i,:Percentage_of_total_1st_purchases],Time_1st_buy[i,:Percentage_of_total_buying_HHs],0,0,0,0,0,0,0,0,ChannelCode] )
+            else	
+                push!(Lift_Buyer_char_template, [string(i),FREQ * string(i),end_week-start_week+1,start_week,start_week+i-1,"BUYER_CHAR_WEEK_FREQUENCY",0,0,0,0,0,Frst_Buy_Frq_Dgtl_std[i,:Percentage_of_total_1st_purchases],Time_1st_buy[i,:Percentage_of_total_buying_HHs],0,0,0,0,0,0,0,0,ChannelCode] )
+            end
+        end
+        for i in 1:(nrow(Time_1st_buy)-1)
+            if i == 10
+                push!(Lift_Buyer_char_template, [string("10+"),"FRQ1388",end_week-start_week+1,start_week,start_week+i-1,"BUYER_CHAR_FREQUENCY",0,0,0,0,0,Frst_Buy_Frq_Dgtl_std[i,:Percentage_of_total_1st_purchases],Time_1st_buy[i,:Percentage_of_total_buying_HHs],0,0,0,0,0,0,0,0,ChannelCode] )
+            else
+                push!(Lift_Buyer_char_template, [string(i),FREQ * string(i),end_week-start_week+1,start_week,start_week+i-1,"BUYER_CHAR_FREQUENCY",0,0,0,0,0,Frst_Buy_Frq_Dgtl_std[i,:Percentage_of_total_1st_purchases],Time_1st_buy[i,:Percentage_of_total_buying_HHs],0,0,0,0,0,0,0,0,ChannelCode] )
+            end
+        end
+    end
+
+    if cfg[:campaign_type] == :google 
+        for i in 1:(nrow(Time_1st_buy)-1)
+                        push!(Lift_Buyer_char_template, [string(i),FREQ * string(i),end_week-start_week+1,start_week,start_week+i-1,"BUYER_CHAR_WEEK_FREQUENCY",0,0,0,0,0,0,Time_1st_buy[i,:Percentage_of_total_buying_HHs],0,0,0,0,0,0,0,0,ChannelCode] )
+        end
+        for i in 1:(nrow(Time_1st_buy)-1)
+                        push!(Lift_Buyer_char_template, [string(i),FREQ * string(i),end_week-start_week+1,start_week,start_week+i-1,"BUYER_CHAR_FREQUENCY",0,0,0,0,0,0,Time_1st_buy[i,:Percentage_of_total_buying_HHs],0,0,0,0,0,0,0,0,ChannelCode] )
+        end
+    end
+
+    TCP0="TCP0"
+    if flag == 1 
+        for i in unique(exposed_buyer_by_week[:iri_week])
+            PCT_LAPSED_BUYERS_T = sum(exposed_buyer_by_week[:PCT_LAPSED_BUYERS])
+            PCT_BRAND_SWITCH_BUYERS_T = sum(exposed_buyer_by_week[:PCT_BRAND_SWITCH_BUYERS])
+            PCT_BRAND_BUYERS_T = sum(exposed_buyer_by_week[:PCT_BRAND_BUYERS])
+            PCT_CATEGORY_BUYERS_T = sum(exposed_buyer_by_week[:PCT_CATEGORY_BUYERS])
+            push!(Lift_Buyer_char_template, ["Total Campaign",TCP0,i - start_week + 1,i,end_week,"BUYER_CHAR_REACH",sum(exposed_buyer_by_week[exposed_buyer_by_week[:iri_week].<=i,:PCT_CATEGORY_BUYERS])/PCT_CATEGORY_BUYERS_T, sum(exposed_buyer_by_week[exposed_buyer_by_week[:iri_week].<=i,:PCT_BRAND_SWITCH_BUYERS])/PCT_BRAND_SWITCH_BUYERS_T, sum(exposed_buyer_by_week[exposed_buyer_by_week[:iri_week].<=i,:PCT_BRAND_BUYERS])/PCT_BRAND_BUYERS_T, sum(exposed_buyer_by_week[exposed_buyer_by_week[:iri_week].<=i,:PCT_LAPSED_BUYERS])/PCT_LAPSED_BUYERS_T,0,0,0,0,0,0,0,0,0,0,0,ChannelCode] )
+            push!(Lift_Buyer_char_template, ["Total Campaign",TCP0,i - start_week + 1,i,end_week,"BUYER_CHAR_EXPOSURES",convert(Float64,exposed_buyer_by_week[exposed_buyer_by_week[:iri_week].==i,:PCT_CATEGORY_BUYERS][1]), convert(Float64,exposed_buyer_by_week[exposed_buyer_by_week[:iri_week].==i,:PCT_BRAND_SWITCH_BUYERS][1]), convert(Float64,exposed_buyer_by_week[exposed_buyer_by_week[:iri_week].==i,:PCT_BRAND_BUYERS][1]), convert(Float64,exposed_buyer_by_week[exposed_buyer_by_week[:iri_week].==i,:PCT_LAPSED_BUYERS][1]),0,0,0,0,0,0,0,0,0,0,0,ChannelCode] )
+        end
+    end
+    brand_buyers_unexposed_cnt= deepcopy(dfo_UnExp);
+    push!(Lift_Buyer_char_template, ["Total Campaign",TCP0,end_week-start_week+1,start_week,end_week,"BUYER_CHAR_NON_EXP_BUYER",0,convert(Int32,ceil(brand_buyers_unexposed_cnt[brand_buyers_unexposed_cnt[:buyer_type] .== "BRAND SWITCHERS",:CNT][1])),convert(Int32,ceil(brand_buyers_unexposed_cnt[brand_buyers_unexposed_cnt[:buyer_type] .== "BRAND BUYERS",:CNT][1])),convert(Int32,ceil(brand_buyers_unexposed_cnt[brand_buyers_unexposed_cnt[:buyer_type] .== "LAPSED BUYERS",:CNT][1])),0,0,0,convert(Int32,ceil(brand_buyers_unexposed_cnt[brand_buyers_unexposed_cnt[:buyer_type] .== "NEW BUYERS",:CNT][1])),convert(Int32,ceil(brand_buyers_unexposed_cnt[brand_buyers_unexposed_cnt[:buyer_type] .== "NON BUYERS",:CNT][1])),convert(Int32,ceil(brand_buyers_unexposed_cnt[brand_buyers_unexposed_cnt[:buyer_type] .== "REPEAT BUYERS",:CNT][1])),convert(Int32,ceil(brand_buyers_unexposed_cnt[brand_buyers_unexposed_cnt[:buyer_type] .== "CATEGORY SWITCHERS",:CNT][1])),0,0,0,0,ChannelCode] )
+    brand_buyers_exposed_cnt= deepcopy(dfo_Exp);
+    push!(Lift_Buyer_char_template, ["Total Campaign",TCP0,end_week-start_week+1,start_week,end_week,"BUYER_CHAR_BUYER",0,convert(Int32,ceil(brand_buyers_exposed_cnt[brand_buyers_exposed_cnt[:buyer_type] .== "BRAND SWITCHERS",:CNT][1])),convert(Int32,ceil(brand_buyers_exposed_cnt[brand_buyers_exposed_cnt[:buyer_type] .== "BRAND BUYERS",:CNT][1])),convert(Int32,ceil(brand_buyers_exposed_cnt[brand_buyers_exposed_cnt[:buyer_type] .== "LAPSED BUYERS",:CNT][1])),0,0,0,convert(Int32,ceil(brand_buyers_exposed_cnt[brand_buyers_exposed_cnt[:buyer_type] .== "NEW BUYERS",:CNT][1])),convert(Int32,ceil(brand_buyers_exposed_cnt[brand_buyers_exposed_cnt[:buyer_type] .== "NON BUYERS",:CNT][1])),convert(Int32,ceil(brand_buyers_exposed_cnt[brand_buyers_exposed_cnt[:buyer_type] .== "REPEAT BUYERS",:CNT][1])),convert(Int32,ceil(brand_buyers_exposed_cnt[brand_buyers_exposed_cnt[:buyer_type] .== "CATEGORY SWITCHERS",:CNT][1])),0,0,0,0,ChannelCode])
+    csv_trial_repeat= deepcopy(dfd_rpt_trl);
+    push!(Lift_Buyer_char_template, ["Total Campaign",TCP0,end_week-start_week+1,start_week,end_week,"BUYER_CHAR_TRIAL_REPEAT",0,0,0,0,0,0,0,0,0,0,0,csv_trial_repeat[csv_trial_repeat[:grouptype] .== "Exposed",:repeaters_percent][1],csv_trial_repeat[csv_trial_repeat[:grouptype] .== "Exposed",:triers_percent][1],0,0,ChannelCode] )
+    push!(Lift_Buyer_char_template, ["Total Campaign",TCP0,end_week-start_week+1,start_week,end_week,"BUYER_CHAR_NON_EXP_TRIAL_REPEAT",0,0,0,0,0,0,0,0,0,0,0,csv_trial_repeat[csv_trial_repeat[:grouptype] .== "Unexposed",:repeaters_percent][1],csv_trial_repeat[csv_trial_repeat[:grouptype] .== "Unexposed",:triers_percent][1],0,0,ChannelCode] )
+    mlift_temp_additional_report=DataFrame(target_competitor_ind = String[], description = String[], upc10 = String[], PRE_sales = String[], POS_sales = String[], VARIATION = String[], time_agg_period = Int64[], channel_code = Int64[], grp_index = Int64[], registration_id = String[], registration_request_id = String[])
+    start_week=parse(Int64,cfg[:start_week])
+    end_week=parse(Int64,cfg[:end_week])
+    time_agg_period=end_week-start_week+1
+    upc_growth_df = deepcopy(dfd_upc_grwth_cnt);
+    agg_upc_growth_df = DataFrame(description=String[],upc10=String[],sales_upc_pre=Float64[],sales_upc_post=Float64[],percentage_sales_upc_pre=Float64[],percentage_sales_upc_post=Float64[],growth_contribution=Float64[]);
+    for i in unique(upc_growth_df[:description])
+        push!(agg_upc_growth_df,[upc_growth_df[upc_growth_df[:description] .== i ,:description][1],
+        upc_growth_df[upc_growth_df[:description] .== i ,:upc10][1],
+        mean(upc_growth_df[upc_growth_df[:description] .== i ,:sales_upc_pre]),
+        mean(upc_growth_df[upc_growth_df[:description] .== i ,:sales_upc_post]),
+        mean(upc_growth_df[upc_growth_df[:description] .== i ,:percentage_sales_upc_pre]),
+        mean(upc_growth_df[upc_growth_df[:description] .== i ,:percentage_sales_upc_post]),
+        mean(upc_growth_df[upc_growth_df[:description] .== i ,:growth_contribution])])
+    end
+    sort!(agg_upc_growth_df,cols=:growth_contribution,rev=true)
+    for i in 1:nrow(agg_upc_growth_df)
+        push!(mlift_temp_additional_report, ["",upc_growth_df[i,1],string(upc_growth_df[i,2]),string(convert(BigInt, trunc(upc_growth_df[i,3]))),string(convert(BigInt, trunc(upc_growth_df[i,4]))),"UPC_GROWTH_CONTRIBUTION",time_agg_period,ChannelCode,0,cfg[:reg_id],cfg[:reg_req_id]] )
+    end
+    fair_share_df = deepcopy(dfd_fr_shr_ndx);
+    fair_share_df[:type] ="";
+    for i in 1:nrow(fair_share_df)
+        if fair_share_df[:product_grp_id][i] == 1   fair_share_df[:type][i] = "advertised" else  fair_share_df[:type][i] = "competitor" end
+    end
+    for i in 1:nrow(fair_share_df)
+        push!(mlift_temp_additional_report, [fair_share_df[i,10],string(fair_share_df[i,2]),"",string(convert(BigInt, trunc(fair_share_df[i,3]))),string(convert(BigInt, trunc(fair_share_df[i,5]))),"FAIR_SHARE_INDEX",time_agg_period,ChannelCode,fair_share_df[i,1],cfg[:reg_id],cfg[:reg_req_id]] )
+    end
+    #################Share of Requirement
+    if cfg[:campaign_type] == :lift || cfg[:campaign_type] == :SamsClub  || cfg[:campaign_type] == :digitallift || cfg[:campaign_type] == :digitalliftuat || cfg[:campaign_type] == :tvlift || cfg[:campaign_type] == :tvliftuat
+        push!(mlift_temp_additional_report, ["","","",string(1 - shr_rqrmnt[shr_rqrmnt[:exposed_flag].==1,:product_group_share][1]),string(shr_rqrmnt[shr_rqrmnt[:exposed_flag].==1,:product_group_share][1]),"BRAND_SHARE_EXPOSED",time_agg_period,ChannelCode,0,cfg[:reg_id],cfg[:reg_req_id]] )
+        push!(mlift_temp_additional_report, ["","","",string(1 - shr_rqrmnt[shr_rqrmnt[:exposed_flag].==0,:product_group_share][1]),string(shr_rqrmnt[shr_rqrmnt[:exposed_flag].==0,:product_group_share][1]),"BRAND_SHARE_NONEXPOSED",time_agg_period,ChannelCode,0,cfg[:reg_id],cfg[:reg_req_id]] )
+    end
+
+    if cfg[:campaign_type] == :lift || cfg[:campaign_type] == :SamsClub  || cfg[:campaign_type] == :digitallift || cfg[:campaign_type] == :digitalliftuat || cfg[:campaign_type] == :tvlift || cfg[:campaign_type] == :tvliftuat
+        for i in sort(imp_week[:iri_week])
+            CumulativeHhs = sum(imp_week[imp_week[:iri_week].<=i,:hhs])
+            push!(Lift_Buyer_char_template, [string(i-start_week+1),TCP0,i-start_week+1,start_week,i,"BUYER_CHAR_IMP_HH_COUNTS",0,0,0,0,0,0,0,0,0,0,0,0,0,imp_week[imp_week[:iri_week].==i,:impressions][1],CumulativeHhs,ChannelCode] )
+        end	
+    end		  
+
+   return mlift_temp_additional_report, Lift_Buyer_char_template
 end
 
 end
